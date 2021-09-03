81c773b7d22b40cec4b6367ecc36b2e8bafbaa42
PythonAddict <classpythonaddike@gmail.com>
2021-09-03 07:29:50 +0530

    Integrated debugger with tui

--- main.py
+++ main.py
@@ -1,26 +1,63 @@
+import sys
 import importlib
+import curses
+from curses import wrapper
+from tui import DebuggerScreen, logo
 
 
-def load(file_name):
+class Debugger:
+    def __init__(self):
+        self.module = None
+        self.file_name = sys.argv[1].split(".")[0]
+
+    def load(self):
+        try:
+            self.module = importlib.import_module(self.file_name.split(".")[0])
+            return f"Loaded module - {self.module.__name__}", 0
+        except BaseException as e:
+            return f"Failed to load module: {e}", 1
+
+    def reload(self):
+        if self.module is None:
+            return "Module has not been loaded!", 1
+
+        try:
+            importlib.reload(self.module)
+            return f"Reloaded module - {self.module.__name__}", 0
+        except BaseException as e:
+            return f"Failed to reload module: {e}", 1
+
+
+def main(win: curses.window):
+
+    # Ensure that screen is large enough
+    assert curses.COLS >= 60, "Screen is not wide enough!"
+    assert curses.LINES >= 20, "Screen is not long enough!"
+
+    curses.cbreak()
+
+    debugger = Debugger()
+    debugger.load()
+
+    screen = DebuggerScreen(
+        win,
+        {
+            "load": debugger.load,
+            "reload": debugger.reload,
+            "exit": exit,
+        },
+        10,  # ascii code for enter key => will be used to select commands
+        logo,
+    )
+
+    screen.listen()
+
+
+if __name__ == "__main__":
+
     try:
-        module = importlib.import_module(file_name.split(".")[0])
-        return f"module loaded", module, [i for i in dir(module) if "__" not in i]
-    except BaseException as e:
-        return f"failed to load module: {e}", 0
-
-
-def reload(module):
-    try:
-        importlib.reload(module)
-        return "module reloaded", [i for i in dir(module) if "__" not in i]
-    except BaseException as e:
-        return f"failed to reload module: {e}", 0
-
-
-def exit():
-    pass
-    # this may or may not need code depending on how the tui will work
-
-
-if "__main__" == __name__:
-    print(load("tes"))
+        wrapper(main)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        raise e
--- tui/screen.py
+++ tui/screen.py
@@ -19,12 +19,7 @@ class DebuggerScreen:
 
         self.current_command_index = 0  # First command is selected
 
-        self.commands_window = curses.newwin(
-            curses.LINES,
-            curses.COLS // 2,
-            0,
-            0
-        )
+        self.commands_window = curses.newwin(curses.LINES, curses.COLS // 2, 0, 0)
         self.output_window = curses.newwin(
             curses.LINES, curses.COLS - (curses.COLS // 2), 0, curses.COLS // 2
         )
@@ -49,7 +44,6 @@ class DebuggerScreen:
         self.commands_window.refresh()
         self.output_window.refresh()
 
-
     def init_colors(self):
         # Default colors
         curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -61,10 +55,11 @@ class DebuggerScreen:
         curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
         # Scheme for python logo
         curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        # Errors
+        curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)
 
         self.commands_window.bkgdset(" ", curses.color_pair(2))
         self.output_window.bkgdset(" ", curses.color_pair(2))
-
 
     def draw_commands_window(self):
         """Initialise commands window with title and command lists"""
@@ -77,12 +72,7 @@ class DebuggerScreen:
 
         # Draw the title
         self.commands_window.addstr(1, 1, new_line, curses.color_pair(4))
-        self.commands_window.addstr(
-            2,
-            1,
-            title,
-            curses.A_BOLD | curses.color_pair(4)
-        )
+        self.commands_window.addstr(2, 1, title, curses.A_BOLD | curses.color_pair(4))
         self.commands_window.addstr(3, 1, new_line, curses.color_pair(4))
 
         # Draw each command, with spaces to fill up empty space
@@ -93,7 +83,6 @@ class DebuggerScreen:
                 "> " + cmd + " " * (curses.COLS // 2 - len("> " + cmd) - 4),
             )
 
-
     def draw_output_window(self):
         """Initialise output window with title and logo"""
 
@@ -101,12 +90,11 @@ class DebuggerScreen:
 
         try:
             self.draw_output_window_logo()
-        except Exception as e:
+        except Exception:
             # Screen is too small for the logo
             self.output_window.clear()
             self.output_window.border()
             self.init_output_window()
-
 
     def init_output_window(self):
         """Initialise output window with title"""
@@ -118,14 +106,8 @@ class DebuggerScreen:
         title = padding + title + padding
 
         self.output_window.addstr(1, 1, line, curses.color_pair(4))
-        self.output_window.addstr(
-            2,
-            1,
-            title,
-            curses.A_BOLD | curses.color_pair(4)
-        )
+        self.output_window.addstr(2, 1, title, curses.A_BOLD | curses.color_pair(4))
         self.output_window.addstr(3, 1, line, curses.color_pair(4))
-
 
     def draw_output_window_logo(self):
         """Draw logo in output window"""
@@ -142,19 +124,13 @@ class DebuggerScreen:
             # and blue on bottom right
             # so `|` is used to separate these two groups in the logo
 
-            self.output_window.addstr(
-                y_pad + ln,
-                1,
-                padding + t1,
-                curses.color_pair(5)
-            )
+            self.output_window.addstr(y_pad + ln, 1, padding + t1, curses.color_pair(5))
             self.output_window.addstr(
                 y_pad + ln,
                 1 + len(t1) + len(padding),
                 t2 + padding,
-                curses.color_pair(4)
+                curses.color_pair(4),
             )
-
 
     def select_command(self, pos: int):
         line = 2 * pos + 6
@@ -166,9 +142,8 @@ class DebuggerScreen:
             line,
             3,
             curses.COLS // 2 - 6,
-            curses.A_STANDOUT | curses.A_ITALIC | curses.color_pair(1)
+            curses.A_STANDOUT | curses.A_ITALIC | curses.color_pair(1),
         )
-
 
     def handle_command(self):
 
@@ -178,9 +153,21 @@ class DebuggerScreen:
         self.init_output_window()
 
         cmds = list(self.commands.keys())
-        handler = self.commands[cmds[self.current_command_index]]
-        handler(self.output_window)
+        cmd = cmds[self.current_command_index]
+        handler = self.commands[cmd]
 
+        try:
+            result = handler()
+        except Exception as e:
+            result = (e.__repr__(), 1)
+
+        error = result[1] != 0  # Check if an error ocurred while running the command.
+        result = result[0]
+
+        color = [curses.color_pair(1), curses.color_pair(6)][error]
+
+        for pos, line in enumerate(result.split("\n")):
+            self.output_window.addstr(pos + 6, 3, ["", "Error - "][error] + line, color)
 
     def listen(self):
         """Start listening for inputs"""
